391fd330b7e0a504dfcd39f763f6307a88ada5bf
AboutTreeFittyy <mathewboland@hotmail.com>
2019-10-24 20:48:05 -0400

    reorganized some functions
    some functions in admin.html now in adminmanager.js

--- AdminManager.js
+++ AdminManager.js
@@ -61,3 +61,70 @@ function normalizeImage(image){
 	//Return the image made on the canvas
 	return canvas.toDataURL("image/jpeg", 0.9);//};
 }
+function changeOrder(id, order){
+	//var orderData = getAdminProjectData();
+	//Get project info of two that are chaning orders
+	var thisTitle = document.getElementById("dds"+id);
+	var otherTitle = document.querySelector("button[value='"+order+"']");	
+	//save the current order of the title clicked
+	var curOrder = thisTitle.value;
+	//swap the values of the projects
+	thisTitle.value = otherTitle.value;
+	otherTitle.value = curOrder;
+	//switch text displayed on buttons
+	thisTitle.innerText = "["+(1+Number(order))+"] "+thisTitle.innerText.substring(4);
+	otherTitle.innerText = "["+(1+Number(curOrder))+"] "+otherTitle.innerText.substring(4);
+	//Okay so actually should just ajax request this when clicked
+	var data = {
+	"from_id" : Number(id),
+	"from_order" : order,
+	"to_id" : Number(otherTitle.id.substring(3)),
+	"to_order" : Number(curOrder)
+	};							
+	var dataString = JSON.stringify(data);
+	$.ajax({
+		url:"changeProjectOrder.php",
+		method: "POST",
+		data: {myData: dataString},
+		success:function(data){
+			alert(data);
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("Problem: "+XMLHttpRequest.status+" "+XMLHttpRequest.statusText);
+		}
+	});
+	//reload the div so that it updates properly
+	setAdminProjectData();
+}
+function printPage(){
+	var toPrint = '';
+	//Add sections with skills that need to be selected
+	setAdminSkillsData();
+	//Now add project data 
+	setAdminProjectData();
+}
+//set printPageContents to run when page loads
+window.onload = printPage;
+/* Function Name: dataURItoBlob
+ * Source: https://stackoverflow.com/questions/6850276/how-to-convert-dataurl-to-file-object-in-javascript
+ * Date Written: May 23, 2017
+ * Author: Matthew
+ */
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+	  ia[i] = byteString.charCodeAt(i);
+  }
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+}
