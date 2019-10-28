7a75d16d7f617c5cefffcb6e4ce13f53e4457d85
AboutTreeFittyy <mathewboland@hotmail.com>
2019-10-28 18:29:57 -0400

    fixed homepage data insertion
    The data for the homepage used to not get overwritten when a new one was inserted but now it does

--- addHomePageData.php
+++ addHomePageData.php
@@ -7,15 +7,21 @@ $type = pathinfo($obj->fileName, PATHINFO_EXTENSION);
 // Allow certain file formats
 $allowedTypes = array('jpg','png','jpeg');
 if(in_array($type, $allowedTypes)){
-	$sql = "INSERT INTO misc (Name, About, Image) VALUES ('$obj->title','$obj->description','$obj->fileName')";
+	$sql = "TRUNCATE TABLE misc";
+	if(mysqli_query($con, $sql)){
+		$msg .= "Previous data deleted.";
+		$sql = "INSERT INTO misc (Name, About, Image) VALUES ('$obj->title','$obj->description','$obj->fileName')";
 		if(mysqli_query($con, $sql)){
-			$msg = "The file has been uploaded successfully.";
+			$msg .= "The file has been uploaded successfully.";
 		}else{
-			$msg = "Unable to upload file path to DB.";
+			$msg .= "Unable to upload file path to DB.";
 		} 
+	}else{
+		$msg .= "Unable to delete old data.";
+	}	
 } else{
 	//invalid file type
-	$msg = "Invalid file type selected, only jpg, png or jpeg allowed.";
+	$msg .= "Invalid file type selected, only jpg, png or jpeg allowed.";
 }
 //Show error/success message
 echo $msg;
